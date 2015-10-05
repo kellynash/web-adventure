8682c8bc2f67f20e9dcd952dccafcc699688cd02
kellynash <kellynash@gmail.com>
2015-10-05 15:07:11 -0600

    adding characters

--- adventure.js
+++ adventure.js
@@ -2,6 +2,7 @@ function Grid(numRow, numCol) {
     this.numRow = numRow;
     this.numCol = numCol;
     this.makeGrid = function () {
+        console.log("makeGrid");
         for (i = 0; i < this.numRow; i++) {
             document.write('<div class="row">');
             for (j = 0; j < this.numCol; j++) {
@@ -15,31 +16,35 @@ function Grid(numRow, numCol) {
 
 var abe;
 var frank;
+var adventure;
+
 
 function newGrid() {
-    abe = new Adventure(0, 0, 3, 3, 3, "<img src=\"small_abe.png\"></img>");
-    frank = new Adventure(3, 3, 2, 2, 3, "<img src=\"frank.png\"></img>")
-    //newAbe = new Adventure (2, 3, 2, 2, 3);
-    var newGrid = new Grid(abe.size, abe.size);
+    abe = new Player(0, 0, "abe", "<img src=\"small_abe.png\"></img>");
+    frank = new Player(3, 3, "<img src=\"frank.png\"></img>");
+    adventure = new Adventure (abe, 10, 10, 10);
+    var newGrid = new Grid(adventure.size, adventure.size);
     newGrid.makeGrid();
-    abe.movChar();
-    abe.target();
-    frank.movChar();
-    frank.target();
+    adventure.movChar();
+    adventure.target();
+    // frank.movChar();
+    // frank.target();
+};
+
+function Player(x, y, name, avatar) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.avatar = avatar;
 };
 
 
-
-
-function Adventure(startY, startX, endY, endX, bound, avatar) {
-    this.startY = startY;
-    this.startX = startX;
+function Adventure(abe, endY, endX, bound) {
+    this.start = abe;
     this.endX = endX;
     this.endY = endY;
     this.bound = bound;
     this.size = bound + 1;
-    this.avatar = avatar;
-
 };
 
 
@@ -56,36 +61,39 @@ Adventure.prototype.target = function(){
 };
 
 Adventure.prototype.movChar = function(){
-    document.getElementById(this.startY.toString() + 
-        this.startX.toString()).innerHTML = this.avatar;
+   
+console.log("movChar: " + this.start.y.toString() + 
+        this.start.x.toString())
+    document.getElementById(this.start.y.toString() + 
+        this.start.x.toString()).innerHTML = this.start.avatar;
 };
 
 Adventure.prototype.hideAbe = function(xChange, yChange){
-    document.getElementById((this.startY + yChange).toString() + 
-        (this.startX + xChange).toString()).innerHTML = "";
+    document.getElementById((this.start.y + yChange).toString() + 
+        (this.start.x + xChange).toString()).innerHTML = "";
 };
 
 Adventure.prototype.mov = function(xDel, yDel, x, y){
-    if(this.startX + x === this.endX
-        && this.startY + y === this.endY) {
-            this.startX += x;
-            this.startY += y;
+    if(this.start.x + x === this.endX
+        && this.start.y + y === this.endY) {
+            this.start.x += x;
+            this.start.y += y;
             this.hideAbe(xDel, yDel);  
             this.killAbe();
             dead.play();
             alert("Whuuthaa!!??");
     }
-    else if (this.startY + y < 0 || this.yCoord + y === this.bound + 1) {
+    else if (this.start.y + y < 0 || this.start.y + y === this.bound + 1) {
         holdon.play();
         alert("D'oh!!!");
     }
-    else if (this.startX + x < 0 || this.startX + x === this.bound + 1) {
+    else if (this.start.x + x < 0 || this.start.x + x === this.bound + 1) {
         coon.play();
         alert("D'oh!!!");
     }
     else {
-        this.startX += x;
-        this.startY += y;
+        this.start.x += x;
+        this.start.y += y;
         this.hideAbe(xDel, yDel);
         this.movChar();
     };
@@ -108,9 +116,9 @@ Adventure.prototype.movRight = function() {
 };
 
 Adventure.prototype.reset = function() {
-    this.hideAbe(0,0);
-    this.startX = 0;
-    this.startY = 0;
+    this.hideAbe(0, 0);
+    this.start.x = 0;
+    this.start.y = 0;
     this.movChar();
     this.target();
 };
